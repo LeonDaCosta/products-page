a544509f4bff994847d9d40441499dcaf7893fb2
LeonDacosta <dacosta.ldc@gmail.com>
2024-08-09 12:20:37 +0100

    Created a single sort function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let itemContainer = document.querySelector("#item-container")
+let itemContainer = document.querySelector("#item-container");
 let listproducts = [];
 
 const fetchProducts = async () => {
@@ -8,36 +8,96 @@ const fetchProducts = async () => {
     throw new Error("Products not found");
   }
 
-  const products =  await data.json();
+  const products = await data.json();
   localStorage.setItem("products", JSON.stringify(products));
   createItem(products);
 };
 
 const formatPrice = (price) => {
-  return new Intl.NumberFormat('en-GB', {minimumFractionDigits: 2, minimumFractionDigits: 2, style: 'currency', currency: 'GBP' }).format(
-    price/100,
+  return new Intl.NumberFormat('en-GB', { minimumFractionDigits: 2, minimumFractionDigits: 2, style: 'currency', currency: 'GBP' }).format(
+    price / 100,
   );
 }
 
+const sortItems = (selector) => {
+  let products = JSON.parse(localStorage.getItem('products'));
+  let sort = localStorage.getItem(selector);
+  let productAttribute = selector.substring(selector.indexOf("_") + 1);
+  let activeSortSelect = "#b" + productAttribute.charAt(0).toUpperCase() + productAttribute.slice(1);
+  localStorage.setItem(selector, sort);
+  let direction = "";
+
+  console.log(selector)
+  console.log(productAttribute)
+  console.log(activeSortSelect)
+
+  products["product_arr"].sort((a, b) => {
+    if (sort == 1) {
+      if (selector == "sort_savings") {
+        if ((a.was_price - a.price) > (b.was_price - b.price)) {
+          return -1;
+        }
+      } else {
+        if (a[productAttribute] > b[productAttribute]) {
+          return -1;
+        }
+      }
+      direction = "up";
+    } else {
+      if (selector == "sort_savings") {
+        if ((a.was_price - a.price) < (b.was_price - b.price)) {
+          return -1;
+        }
+      } else {
+        if (a[productAttribute] < b[productAttribute]) {
+          return -1;
+        }
+      }
+      direction = "down";
+    }
+    return 0;
+  });
+
+  if (sort == 1) {
+    sort = 0;
+  } else {
+    sort = 1;
+  }
+
+  localStorage.setItem(selector, sort);
+  itemContainer.innerHTML = '';
+  activeSort(activeSortSelect);
+  sortDirection(activeSortSelect, direction);
+  createItem(products);
+}
+
+// Deprecated to be removed
 const sortPrice = () => {
   let products = JSON.parse(localStorage.getItem('products'));
   let sort = localStorage.getItem("sort_price"); // Toggle sort direction
   localStorage.setItem("sort_price", sort);
 
+  let selector = "sort_price";
+
+  console.log(selector.indexOf("_"))
+
+  console.log(selector.substring(selector.indexOf("_") + 1))
+  console.log(selector.substring(selector.indexOf("_") + 1).charAt(0).toUpperCase())
+
   products["product_arr"].sort((a, b) => {
-    if(sort == 1){
-      if(a.price > b.price) {
+    if (sort == 1) {
+      if (a['price'] > b['price']) {
         return -1;
       }
     } else {
-      if(a.price < b.price) {
+      if (a['price'] < b['price']) {
         return -1;
       }
     }
     return 0;
   });
 
-  if(sort == 1){
+  if (sort == 1) {
     sort = 0;
   } else {
     sort = 1;
@@ -49,24 +109,25 @@ const sortPrice = () => {
   createItem(products);
 }
 
+// Deprecated to be removed
 const sortReview = () => {
   let products = JSON.parse(localStorage.getItem('products'));
   let sort = localStorage.getItem("sort_review"); // Toggle sort direction
   localStorage.setItem("sort_review", sort);
   products["product_arr"].sort((a, b) => {
-    if(sort == 1) {
-      if(a.reviews > b.reviews) {
+    if (sort == 1) {
+      if (a.reviews > b.reviews) {
         return -1;
-      }  
+      }
     } else {
-      if(a.reviews < b.reviews) {
+      if (a.reviews < b.reviews) {
         return -1;
-      }         
+      }
     }
     return 0;
   });
 
-  if(sort == 1){
+  if (sort == 1) {
     sort = 0;
   } else {
     sort = 1;
@@ -78,25 +139,26 @@ const sortReview = () => {
   createItem(products);
 }
 
+// Deprecated to be removed
 const sortName = () => {
   let products = JSON.parse(localStorage.getItem('products'));
   let sort = localStorage.getItem("sort_name"); // Toggle sort direction
   localStorage.setItem("sort_name", sort);
 
   products["product_arr"].sort((a, b) => {
-    if(sort == 1) {
-      if(a.name > b.name) {
+    if (sort == 1) {
+      if (a.name > b.name) {
         return -1;
-      } 
+      }
     } else {
-      if(a.name < b.name) {
+      if (a.name < b.name) {
         return -1;
       }
     }
     return 0;
   });
 
-  if(sort == 1){
+  if (sort == 1) {
     sort = 0;
   } else {
     sort = 1;
@@ -108,25 +170,26 @@ const sortName = () => {
   createItem(products);
 }
 
+// Deprecated to be removed
 const sortSavings = () => {
   let products = JSON.parse(localStorage.getItem('products'));
   let sort = localStorage.getItem("sort_savings"); // Toggle sort direction
   localStorage.setItem("sort_savings", sort);
 
   products["product_arr"].sort((a, b) => {
-    if(sort == 1) {
-      if((a.was_price - a.price) > (b.was_price - b.price)) {
+    if (sort == 1) {
+      if ((a.was_price - a.price) > (b.was_price - b.price)) {
         return -1;
       }
     } else {
-      if((a.was_price - a.price) < (b.was_price - b.price)) {
+      if ((a.was_price - a.price) < (b.was_price - b.price)) {
         return -1;
       }
     }
     return 0;
   });
 
-  if(sort == 1){
+  if (sort == 1) {
     sort = 0;
   } else {
     sort = 1;
@@ -140,18 +203,48 @@ const sortSavings = () => {
 
 const activeSort = (button) => {
   document.querySelector(button).classList.add("sort-button-active");
-  if(button != "#bPrice") document.querySelector("#bPrice").classList.remove("sort-button-active");
-  if(button != "#bReview") document.querySelector("#bReview").classList.remove("sort-button-active");
-  if(button != "#bName") document.querySelector("#bName").classList.remove("sort-button-active");
-  if(button != "#bSavings") document.querySelector("#bSavings").classList.remove("sort-button-active");
+  if (button != "#bPrice") document.querySelector("#bPrice").classList.remove("sort-button-active");
+  if (button != "#bReviews") document.querySelector("#bReviews").classList.remove("sort-button-active");
+  if (button != "#bName") document.querySelector("#bName").classList.remove("sort-button-active");
+  if (button != "#bSavings") document.querySelector("#bSavings").classList.remove("sort-button-active");
+}
+
+const sortDirection = (selectedButton, direction) => {
+  let button = document.querySelector(selectedButton);
+  let arrow = document.createElement('i');
+  arrow.className = "arrow " + direction;
+
+  let text = button.firstChild;
+  let iTag = button.getElementsByTagName('i')[0];
+
+  let buttonsContainer = document.getElementById("item-filters-container");
+  let allButtons = buttonsContainer.getElementsByTagName("button")
+
+  for (let i = 0; i < allButtons.length; i++) {
+    if (allButtons[i].id != selectedButton.slice(1)) {
+      console.log("remove")
+      if (allButtons[i].getElementsByTagName('i')[0]) {
+        allButtons[i].removeChild(allButtons[i].getElementsByTagName('i')[0]);
+      }
+    }
+  }
+
+  if (iTag) {
+    button.innerHTML = '';
+    button.appendChild(text);
+    button.appendChild(arrow);
+  } else {
+    button.appendChild(text);
+    button.appendChild(arrow);
+  }
 }
 
 const createItem = (products) => {
   products["product_arr"].forEach(item => {
-      let newItem = document.createElement('div');
-      let wasPrice = item.was_price ? `Was <span class="">${formatPrice(item.was_price)}</span>` : '';
-      let review = item.reviews ? `${item.reviews} Review Score` : '';
-      newItem.innerHTML = `
+    let newItem = document.createElement('div');
+    let wasPrice = item.was_price ? `Was <span class="">${formatPrice(item.was_price)}</span>` : '';
+    let review = item.reviews ? `${item.reviews} Review Score` : '';
+    newItem.innerHTML = `
         <div class="item-image-container"><img src="img/${item.img}.jpg" /></div>
         <h3>${item.name}</h3>
         <div class="item-price-container">${formatPrice(item.price)}</div>
@@ -160,12 +253,12 @@ const createItem = (products) => {
         <button class="add-button" onclick="addToBasket('${item.name}')">Add To Basket</button>
       `;
 
-      itemContainer.appendChild(newItem)
-    });
+    itemContainer.appendChild(newItem);
+  });
 }
 
 const addToBasket = (item) => {
-    console.log(`${item}, added to basket.`);
-} 
+  console.log(`${item}, added to basket.`);
+}
 
 fetchProducts();
